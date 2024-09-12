5a15da3afdc1323aac0beb6c0e1b264231804235
stlouisn <stlouisn@hotmail.com>
2024-09-12 11:31:59 -0300

    Update Constants.java

--- src/main/java/dev/axesareweapons/Constants.java
+++ src/main/java/dev/axesareweapons/Constants.java
@@ -1,5 +1,8 @@
 package dev.axesareweapons;
 
+import net.minecraft.item.AxeItem;
+import net.minecraft.item.Item;
+
 public final class Constants {
 
   public static final String MOD_ID = "axesareweapons";
