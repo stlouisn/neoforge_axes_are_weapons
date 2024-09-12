0c953c626df27f5066ac3d06537a19c95a5f5817
stlouisn <stlouisn@hotmail.com>
2024-09-12 11:31:14 -0300

    Update Constants.java

--- src/main/java/dev/axesareweapons/Constants.java
+++ src/main/java/dev/axesareweapons/Constants.java
@@ -3,4 +3,6 @@ package dev.axesareweapons;
 public final class Constants {
 
   public static final String MOD_ID = "axesareweapons";
+
+  public static boolean isAxe(Item item) { return item instanceof AxeItem; }
 }
