db5cf6a8f76fcee24aedce8a18a07f98e5f4fd5f
stlouisn <stlouisn@hotmail.com>
2024-09-12 11:35:20 -0300

    Update and rename Functions.java to ModFunctions.java

--- src/main/java/dev/axesareweapons/utils/ModFunctions.java
+++ src/main/java/dev/axesareweapons/utils/ModFunctions.java
@@ -3,7 +3,7 @@ package dev.axesareweapons.utils;
 import net.minecraft.item.AxeItem;
 import net.minecraft.item.Item;
 
-public final class Constants {
+public final class ModFunctions {
 
   public static boolean isAxe(Item item) { return item instanceof AxeItem; }
 }
