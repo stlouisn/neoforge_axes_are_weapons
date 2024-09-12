72603e2121b636c66a6b87c7fbfc413fadb768a8
stlouisn <stlouisn@hotmail.com>
2024-09-12 11:33:09 -0300

    Create Functions.java

---
+++ src/main/java/dev/axesareweapons/utils/Functions.java
@@ -0,0 +1,9 @@
+package dev.axesareweapons.utils;
+
+import net.minecraft.item.AxeItem;
+import net.minecraft.item.Item;
+
+public final class Constants {
+
+  public static boolean isAxe(Item item) { return item instanceof AxeItem; }
+}
