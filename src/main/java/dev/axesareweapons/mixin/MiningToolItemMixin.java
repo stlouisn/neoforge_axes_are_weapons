85760eb89216d963f6f437f39663684f3b88e628
stlouisn <stlouisn@hotmail.com>
2024-10-24 08:40:02 -0300

    Update MiningToolItemMixin.java
    update to 1.21.3

--- src/main/java/dev/axesareweapons/mixin/MiningToolItemMixin.java
+++ src/main/java/dev/axesareweapons/mixin/MiningToolItemMixin.java
@@ -3,17 +3,16 @@ package dev.axesareweapons.mixin;
 import static dev.axesareweapons.utils.ModFunctions.isAxe;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import net.minecraft.item.Item;
 import net.minecraft.item.MiningToolItem;
-import net.minecraft.item.ToolItem;
-import net.minecraft.item.ToolMaterial;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(value = MiningToolItem.class)
-public abstract class MiningToolItemMixin extends ToolItem {
+public abstract class MiningToolItemMixin extends Item {
 
-  public MiningToolItemMixin(ToolMaterial toolMaterial, Settings settings) {
-    super(toolMaterial, settings);
+	public MiningToolItemMixin(Settings settings) {
+		super(settings);
   }
 
   @ModifyExpressionValue(method = "postDamageEntity", at = @At(value = "CONSTANT", args = "intValue=2"))
